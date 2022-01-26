473667e2ac45e0f7ba02888f416bfe22317ec534
Younho Choo <yo+github@younho9.com>
2022-01-26 16:29:11 +0900

    bootstrap.sh: link step both

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,13 +7,13 @@ if [ -d $DOTFILES_DIR ]; then
     git pull origin main
 else
     git clone git@github.com:younho9/dotfiles.git $DOTFILES_DIR
-
-    ln -sf "$DOTFILES_DIR/.aliases" "$HOME/.aliases"
-    ln -sf "$DOTFILES_DIR/.exports" "$HOME/.exports"
-    ln -sf "$DOTFILES_DIR/.gitconfig" "$HOME/.gitconfig"
-    ln -sf "$DOTFILES_DIR/.gitconfig.company" "$HOME/.gitconfig.company"
-    ln -sf "$DOTFILES_DIR/.gitignore_global" "$HOME/.gitignore_global"
-    ln -sf "$DOTFILES_DIR/.gitmessage.txt" "$HOME/.gitmessage.txt"
-    ln -sf "$DOTFILES_DIR/.p10k.zsh" "$HOME/.p10k.zsh"
-    ln -sf "$DOTFILES_DIR/.zshrc" "$HOME/.zshrc"
 fi
+
+ln -sf "$DOTFILES_DIR/.aliases" "$HOME/.aliases"
+ln -sf "$DOTFILES_DIR/.exports" "$HOME/.exports"
+ln -sf "$DOTFILES_DIR/.gitconfig" "$HOME/.gitconfig"
+ln -sf "$DOTFILES_DIR/.gitconfig.company" "$HOME/.gitconfig.company"
+ln -sf "$DOTFILES_DIR/.gitignore_global" "$HOME/.gitignore_global"
+ln -sf "$DOTFILES_DIR/.gitmessage.txt" "$HOME/.gitmessage.txt"
+ln -sf "$DOTFILES_DIR/.p10k.zsh" "$HOME/.p10k.zsh"
+ln -sf "$DOTFILES_DIR/.zshrc" "$HOME/.zshrc"
