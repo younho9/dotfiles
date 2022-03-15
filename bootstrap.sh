eb2f10a6853a1321afd41ff8238b00b81d1ade18
Younho Choo <yo+github@younho9.com>
2022-03-15 17:24:25 +0900

    style: indent using tabs

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,10 +3,10 @@
 export DOTFILES_DIR="$HOME/repositories/github/public/dotfiles"
 
 if [ -d $DOTFILES_DIR ]; then
-    cd "$(dirname "${BASH_SOURCE}")";
-    git pull origin main
+	cd "$(dirname "${BASH_SOURCE}")";
+	git pull origin main
 else
-    git clone git@github.com:younho9/dotfiles.git $DOTFILES_DIR
+	git clone git@github.com:younho9/dotfiles.git $DOTFILES_DIR
 fi
 
 ln -sf "$DOTFILES_DIR/.aliases" "$HOME/.aliases"
